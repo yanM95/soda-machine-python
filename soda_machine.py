dab1dd3e91ff4f65f381387e5c73bc2b54960ec0
yanM95 <40393174+yanM95@users.noreply.github.com>
2022-03-28 14:22:20 +0200

    inventory v2

--- soda_machine.py
+++ soda_machine.py
@@ -7,6 +7,8 @@ Created on Fri Mar 25 18:55:56 2022
 from item import Item
 
 class SodaMachine:
+    
+    _instance = None
 
     def __init__(self):
 
@@ -16,13 +18,20 @@ class SodaMachine:
             Item("Fanta", 15, 3),
         ]
 
-        self.money_inserted = 0.00
+        self.money_inserted = 0
 
-    def display_items(self):
+    def display_items (self) :
         for code, item in enumerate(self.items, start = 1):
-            print(f"n {code} - {item.name} - {item.price} NOK - {item.inventory} remaining")
+            print(f"n°{code} - {item.name} - {item.price} NOK - {item.inventory} remaining")
 
-    def insert_money(self, money):
-            if money <= 0:
-                raise ValueError
-            self.money_inserted += money
+    def insert_money (self, money) :
+        if money <= 0:
+            raise ValueError
+        self.money_inserted += money
+    
+
+    def __new__(class_, *args, **kwargs):
+        if not isinstance(class_._instance, class_):
+            class_._instance = object.__new__(class_, *args, **kwargs)
+        return class_._instance
+        
