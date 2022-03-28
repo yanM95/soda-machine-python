3512fa6d55ccea7584119e451713fac6793e3f64
yanM95 <40393174+yanM95@users.noreply.github.com>
2022-03-28 15:29:45 +0200

    + Inventory v4

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from soda_machine import SodaMachine
 
 def main():
 
-    soda_machine = SodaMachine()
+    soda_machine = SodaMachine.get_instance()
     soda_machine.display_items()
 
     while True:
@@ -34,11 +34,11 @@ def main():
                 continue
             else:
                 break
-    print(f"\n Giving \"{item.name}\" out.")
-    print(f"Returning {soda_machine.money_inserted - item.price:.0f} NOK to customer.\n")
+    print(f"\n  Giving \"{item.name}\" out.")
+    print(f"  Returning {soda_machine.money_inserted - item.price:.0f} NOK to customer.\n")
     item.inventory -= 1
-
-    while item.inventory != 0 :        
+    
+    if item.inventory != 0 :        
         main()
 
     return 0
